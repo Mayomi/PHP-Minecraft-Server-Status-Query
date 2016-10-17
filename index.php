d1c2559b2470dabe435e728baabe369ea50b9b85
Mayomi <Mayomi@baka.wang>
2016-10-17 22:27:12 +0900

    删除无用资源

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo $c_desc; ?>">
-    <link rel="shortcut icon" href="img/favicon.png">
 
     <title><?php echo $c_name; ?> - <?php echo $c_desc; ?></title>
 
